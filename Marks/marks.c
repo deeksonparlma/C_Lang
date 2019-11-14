a94ca0eb31c8fb40dda5956eb2d100716c6e5f0a
deeksonparlma <dicksonparlma@gmail.com>
2019-11-14 19:49:43 +0300

    sorted the marks in descending order

--- Marks/marks.c
+++ Marks/marks.c
@@ -9,7 +9,7 @@ Make recommendations
 #include <string.h>
 int main(int argc, char const *argv[]) {
   int ArraySize = 8,i=0;
-  int MarksArray[ArraySize],Mark,Error=0,FailCount=0;
+  int MarksArray[ArraySize],Mark,Error=0,FailCount=0,apples;
   float Average,sum=0;
   char Grade;
   char UnitArray[8][50]={"Introduction to programming","Maths for science","Discrete Structures","Software applications","Physics","Computer networks","Communicaton SKills","Development Studies"};
@@ -79,15 +79,29 @@ int main(int argc, char const *argv[]) {
     else if (FailCount>5) {
       printf("\nStatus : Discontinued\n");
     }
+    for (i=0; i<ArraySize; i++) {
+      for (int j=0; j<ArraySize; j++) {
+        if (MarksArray[j]<MarksArray[i]) {
+          apples=MarksArray[i];
+          MarksArray[i]=MarksArray[j];
+          MarksArray[j]=apples;
+        }
+      }
+    }
 
   //output
     printf("Failed = %d\n",FailCount );
     printf("Sum = %.2f\n",sum );
     printf("Average = %.2f\n",Average);
     printf("Grade = %c\n", Grade);
+    printf("Marks in descending order :");
+    for (i=0; i<ArraySize; i++) {
+    printf("\t %d ",MarksArray[i]);
+    }
   }
   else{
     printf("Error check your inputs\n");
   }
+  printf("\n");
   return 0;
 }
